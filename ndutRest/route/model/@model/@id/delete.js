4f793603d83f307735b8b32af736ce91c7ea8ca3
Ardhi Lukianto <ardhi@lukianto.com>
2022-10-03 07:21:19 +0700

    Updates

--- ndutRest/route/model/@model/@id/delete.js
+++ ndutRest/route/model/@model/@id/delete.js
@@ -1,7 +1,7 @@
 module.exports = {
   handler: async function (request, reply) {
     if (!request.isAdmin) throw this.Boom.forbidden('onlyAminCanDoThis', { ndut: 'ndutAdmin' })
-    const { handler } = await this.ndutRest.helper.modelAsRemoveRoute({ model: request.params.model })
+    const { handler } = await this.ndutRest.helper.modelAsRemoveRoute({ alias: request.params.model })
     return await handler.call(this, request, reply)
   },
   schema: {
--- ndutRest/route/model/@model/post.js
+++ ndutRest/route/model/@model/post.js
@@ -1,7 +1,7 @@
 module.exports = {
   handler: async function (request, reply) {
     if (!request.isAdmin) throw this.Boom.forbidden('onlyAminCanDoThis', { ndut: 'ndutAdmin' })
-    const { handler } = await this.ndutRest.helper.modelAsCreateRoute({ model: request.params.model })
+    const { handler } = await this.ndutRest.helper.modelAsCreateRoute({ alias: request.params.model })
     return await handler.call(this, request, reply)
   },
   schema: {
